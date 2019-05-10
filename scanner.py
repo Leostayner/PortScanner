b6bac342961fb0e62ad4f276387ca5d0f1a349fa
Leonardo Medeiros <leostayner@gmail.com>
2019-05-09 21:01:43 -0300

    codigo melhorado: suporte a host name e redução no tempo de scaneamento

--- scanner.py
+++ scanner.py
@@ -1,7 +1,5 @@
 import sys
-import socket
 import time
-import argparse
 import nmap
 
 def menu():
@@ -21,80 +19,95 @@ def menu():
     print()
     return value
 
+
 def _exit():
     sys.exit()
 
-def pePro(ports):
-    if(" " in ports):
-        ports = [int(x) for x in ports.split(" ")]
 
-    if("," in ports):
-        ports = [int(x) for x in ports.split(",")]
+def pePro(ports_str):
+    try:
+        if("," in ports_str):
+            ports_str = ports_str.replace(" ", "")
+            ports_list = [int(x) for x in ports_str.split(",")]
+        
+        elif(" " in ports_str):
+            ports_str = ports_str.replace(" ", ",")
+            ports_list = [int(x) for x in ports_str.split(",")]
+        
+        elif("-" in ports_str ):
+            tmp   = [int(x) for x in ports_str.split("-")]
+            ports_list = [x for x in range(tmp[0], tmp[1] + 1)]
+        
+        elif(ports_str.isdigit()):
+            ports_list = [int(ports_str)]
 
-    elif("-" in ports ):
-        tmp   = [int(x) for x in ports.split("-")]
-        ports = [x for x in range(tmp[0], tmp[1] + 1)]
-    
-    elif(ports.isdigit()):
-        return [int(ports)]
+        elif(ports_str == ""):
+            ports_str = "1-65534"
+            ports_list = [x for x in range (1, 65534)]
 
-    elif(ports == ""):
-        ports = [x for x in range (1, 65534)]
+        else:
+            raise Exception("Error: Invalid input format\n"
+                            "Ex. input: 80, 81 or 80 81 or 1-100")
 
-    else:
-        raise Exception("Error: Invalid port format")
-    
-    return ports
+    except Exception as err:
+        raise Exception("Error: Invalid input format\n"
+                        "Ex. input: 80, 81 or 80 81 or 1-100")
 
-def tcp_scanner(target, ports):
+    return ports_str, ports_list
+
+
+def tcp_scanner(target, ports_str, ports_list):
     time_start = time.time() 
     nm = nmap.PortScanner()
     
     print("TCP Port Scanner")
-    print("Target: {0}".format(target) )
-    print("PORT    STATE    SERVICE")
-   
-    nm.scan(target)
     
-    for port in ports:
-        try:
-            lport = nm[target]['tcp'][port]
-            st1 = str(port) + (7 - len( str(port) ) ) * " "
-            st2 = lport["state"] + (8 - len(lport["state"]) ) * " "
-            st3 = lport["name"]
-
-            print(st1, st2, st3)
-                          
-        except Exception as err:
-            pass
+    nm.scan(target, ports_str)
     
-    print("\nScanned in {0:.2f} seconds \n".format(time.time() - time_start))
+    for target in nm.all_hosts():
+        print("Target: {0}".format(target) )
+        print("PORT    STATE    SERVICE")
+
+        for port in ports_list:
+            try:
+                lport = nm[target]['tcp'][port]
+                st1 = str(port) + (7 - len( str(port) ) ) * " "
+                st2 = lport["state"] + (8 - len(lport["state"]) ) * " "
+                st3 = lport["name"]
+
+                print(st1, st2, st3)
+                            
+            except Exception as err:
+                pass
+        
+        print("\nScanned in {0:.2f} seconds \n".format(time.time() - time_start))
 
 
-def udp_scanner(target, ports):
-    time_start = time.time()
+def udp_scanner(target, ports_str, ports_list):
+    time_start = time.time() 
     nm = nmap.PortScanner()
     
-    print("TCP Port Scanner")
-    print("Target: {0}".format(target) )
-    print("PORT    STATE    SERVICE")
+    print("UDP Port Scanner")
     
-    nm.scan(target)
-
-    for port in ports:
-        try:
-            lport = nm[target]['udp'][port]
-            st1 = str(port) + (7 - len( str(port) ) ) * " "
-            st2 = lport["state"] + (8 - len(lport["state"]) ) * " "
-            st3 = lport["name"]
-
-            print(st1, st2, st3)
-
-        except Exception as err:
-            pass
+    nm.scan(target, ports_str)
     
-    print("\nScanned in {0:.2f} seconds \n".format(time.time() - time_start))
+    for target in nm.all_hosts():
+        print("Target: {0}".format(target) )
+        print("PORT    STATE    SERVICE")
 
+        for port in ports_list:
+            try:
+                lport = nm[target]['udp'][port]
+                st1 = str(port) + (7 - len( str(port) ) ) * " "
+                st2 = lport["state"] + (8 - len(lport["state"]) ) * " "
+                st3 = lport["name"]
+
+                print(st1, st2, st3)
+                            
+            except Exception as err:
+                pass
+        
+        print("\nScanned in {0:.2f} seconds \n".format(time.time() - time_start))
 
 
 def network_scanner():
@@ -130,12 +143,12 @@ def network_Portscanner():
     nm.scan(input("Network IP: "))
 
     ports  = input("Port(s) (default: 1-65534): ")
-    ports  = pePro(ports)
+    ports_str, ports_list  = pePro(ports)
     print()
     
 
     for host in nm.all_hosts():
-        tcp_scanner(host, ports)
+        tcp_scanner(host, ports_str, ports_list)
             
 
 def main(): 
@@ -147,13 +160,13 @@ def main():
             value = menu()
             
             if value in range(2):
-                target = input("Target Ip Addres: ")
+                target = input("Target IP or Host Name: ")
                 ports  = input("Port(s) (default: 1-65534): ")
                 print()
 
-                ports = pePro(ports)
+                ports_str, ports_list = pePro(ports)
 
-                list_functions_2ag[value](target, ports)
+                list_functions_2ag[value](target, ports_str, ports_list)
             
             if value in range(2, 6):
                 list_functions[value - 2]()
